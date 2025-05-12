bd16e0f39305540bb729808295651e0c56bec478
Dominic Zahn <dominic.zahn@iosb.fraunhofer.de>
2025-05-12 14:33:36 +0200

    Added exercise 1 content

---
+++ Exercise1/devsetup/src/facepalm/scripts/facepalm8.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+import rospy, time
+import subprocess #import Popen
+
+from std_msgs.msg import String
+
+gazebo = subprocess.Popen("roslaunch reemc_gazebo reemc_empty_world.launch".split()
+
+gazebo = subprocess.Popen("roslaunch reemc_controller_configuration joint_trajectory_controllers.launch".split())
+
+rospy.init_node('facepalmer', anonymous=True)
+rate = rospy.Rate(10)
+
+while not rospy.is_shutdown()
+    rate.sleep()
