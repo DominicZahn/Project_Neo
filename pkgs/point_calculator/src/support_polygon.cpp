b47bb1ac61ca4c0738d9abe03eaa87b1ad770973
Dominic Zahn <dominic.zahn@iosb.fraunhofer.de>
2025-07-28 11:42:48 +0200

    Trying to build a subscriber

--- pkgs/point_calculator/src/support_polygon.cpp
+++ pkgs/point_calculator/src/support_polygon.cpp
@@ -8,10 +8,34 @@ typedef model::segment<Point> Segment;
 #include <rclcpp/rclcpp.hpp>
 #include <ros_gz_interfaces/msg/contacts.hpp>
 
-int main(int argc, char** argv) {
+class MinimalSubscriber : public rclcpp::Node {
+   public:
+    MinimalSubscriber() : Node("min_sub") {
+        auto callback =
+            [this](ros_gz_interfaces::msg::Contacts contacts) -> void {
+            RCLCPP_INFO(this->get_logger(), "contacts received");
+        };
+            this->create_subscription<ros_gz_interfaces::msg::Contacts>(
+                "/h1/left_contact_force", 10, callback);
+    }
+
+   private:
+    rclcpp::Subscription<ros_gz_interfaces::msg::Contacts>::SharedPtr
+        subscription;
+};
+
+int main(int argc, char **argv) {
     rclcpp::init(argc, argv);
-    
-    
+
+    //rclcpp::spin(std::make_shared<MinimalSubscriber>());
+
+    auto subNode = std::make_shared<rclcpp::Node>("subNode");
+    auto logger = subNode->get_logger();
+    [logger](ros_gz_interfaces::msg::Contacts contacts) -> void {
+        RCLCPP_INFO(logger, "contacts received");
+    };
+    auto subscribtion = subNode->create_subscription<ros_gz_interfaces::msg::Contacts>("/h1/left_contact_force", 10, callback);
+    rclcpp::spin(subNode);
 
     rclcpp::shutdown();
     return 0;
