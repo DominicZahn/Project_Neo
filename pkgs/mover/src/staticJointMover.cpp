473e0cab6aa55d4721d5e116d8e1a3cfb41d6f76
Dominic Zahn <dominic.zahn@iosb.fraunhofer.de>
2025-07-31 10:04:17 +0200

    Pushing for nlopt and rbdl support

---
+++ pkgs/mover/src/staticJointMover.cpp
@@ -0,0 +1,13 @@
+#include <cstdio>
+#include <rbdl/rbdl.h>
+
+using namespace RigidBodyDynamics;
+
+int main(int argc, char ** argv)
+{
+  (void) argc;
+  (void) argv;
+
+  printf("hello world mover package\n");
+  return 0;
+}
