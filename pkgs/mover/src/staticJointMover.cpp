230ec49140204a073f397c0a5298fcab56e157fd
Dominic Zahn <dominic.zahn@student.kit.edu>
2025-08-03 14:42:56 +0200

    fixed rbdl support

--- pkgs/mover/src/staticJointMover.cpp
+++ pkgs/mover/src/staticJointMover.cpp
@@ -8,6 +8,8 @@ int main(int argc, char ** argv)
   (void) argc;
   (void) argv;
 
+  rbdl_check_api_version(RBDL_API_VERSION);
+  rbdl_print_version();
   printf("hello world mover package\n");
   return 0;
 }
