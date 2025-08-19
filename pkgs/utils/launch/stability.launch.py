f42c81bf774a18de25758edc5479801c6bac4bb2
Dominic Zahn <dominic.zahn@iosb.fraunhofer.de>
2025-08-19 14:58:43 +0200

    Added launch file with option for rviz and gazebo

--- pkgs/utils/launch/stability.launch.py
+++ pkgs/utils/launch/stability.launch.py
@@ -1,20 +1,43 @@
 import os
 from launch import LaunchDescription
 from launch_ros.actions import Node
-from launch.actions import IncludeLaunchDescription
+from launch.substitutions import LaunchConfiguration
+from launch.actions import (IncludeLaunchDescription, DeclareLaunchArgument, OpaqueFunction)
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
-    gazeboLaunchFile = os.path.join(
-        get_package_share_directory('ros_gz_h1_bringup'),
-        'launch',
-        'h1_gazebo_sim.launch.py'
+
+    arg = DeclareLaunchArgument(
+        'sim',
+        default_value='gz',
+        choices=['gz', 'rviz']
     )
+
+    def chooseLaunchFile(context, *args, **kwargs):
+        bringupDir = get_package_share_directory('ros_gz_h1_bringup')
+        gazeboLaunchFile = os.path.join(
+            bringupDir,
+            'launch',
+            'h1_gazebo_sim.launch.py')
+        rvizLaunchFile = os.path.join(
+            bringupDir,
+            'launch',
+            'h1_rviz.launch.py')
+        launchFile = ""
+        sim_arg_val = LaunchConfiguration("sim").perform(context)
+        if sim_arg_val == "gz":
+            launchFile = gazeboLaunchFile
+        else:
+            launchFile = rvizLaunchFile
+
+        return IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(launchFile)
+        ),        
+
     return LaunchDescription([
-        IncludeLaunchDescription(
-            PythonLaunchDescriptionSource(gazeboLaunchFile)
-        ),
+        arg,
+        OpaqueFunction(function=chooseLaunchFile),
         Node(
             package='point_calculator',
             executable='center_of_mass',
