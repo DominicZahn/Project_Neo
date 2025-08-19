cd63243f649f1a68fd087767fa7f47e0d157261f
Dominic Zahn <dominic.zahn@iosb.fraunhofer.de>
2025-08-19 10:46:48 +0200

    heatmap for stability visualization

--- docu/projectNeo/todos.md
+++ docu/projectNeo/todos.md
@@ -9,6 +9,7 @@
 - [x] optimize *com* calculator to get faster responses
     - using rbdl to read urdf file
 - [ ] (optional) fix permissions
+- [ ] add parameter to launch file to activate gazebo (else rviz)
 - [x] write static stability checker
     - [x] calculate support polygon from feet (generating polygon from multiple points)
     - [x] check if center of mass is inside polygon of support (point inside polyon problem)
@@ -20,18 +21,19 @@
     - [x] read position from txt file
     - [x] move robot to that postion and stop if stability is insufficient
     - [x] formulate optimization problem
-    - [ ] use optimization to find new joint configurations and move slowly towards it
+    ~- [ ] use optimization to find new joint configurations and move slowly towards it~
 
     - [ ] fix to small increment problem (**if thats even possible**)
+    -> use integer arithmetic
 ### Stability Criteria
 - [ ] visualize different criteria (heatmap of a given PoS)
 - [x] fix PoS
     - [x] make more stable over time -> *more points more stable*
-    - [ ] project to ground CoM and PoS -> *to test*
-### Optimal Control
+### Dodge Control
+> Using rbdl and nlopt to let h1 dodge away from a given postion at a given point in time.
 - [ ] write pseudo formulation for optimal control problem
-- [ ] build inverse kinematics component with *Optimal Control* framework
-- [ ] bring robot in evasion pose with inverse kinematics component
+- [ ] spawn robot in pose
+- [ ] visualize results of optimization
 
 # Q&A
 - why incrementation problem????? -> integer arithmetic, multiplie of low double
--- pkgs/point_calculator/src/support_polygon.cpp
+++ pkgs/point_calculator/src/support_polygon.cpp
@@ -5,19 +5,26 @@ typedef bg::model::point<double, 2, bg::cs::cartesian> Point;
 typedef bg::model::polygon<Point> Polygon;
 typedef bg::model::segment<Point> Segment;
 
+#include <ctime>
 #include <geometry_msgs/msg/point_stamped.hpp>
 #include <iostream>
 #include <map>
 #include <rclcpp/rclcpp.hpp>
 #include <ros_gz_interfaces/msg/contacts.hpp>
+#include <sensor_msgs/msg/point_cloud2.hpp>
+typedef sensor_msgs::msg::PointCloud2 msgPointCloud2;
+#include <sensor_msgs/point_cloud2_iterator.hpp>
+typedef sensor_msgs::PointCloud2Iterator<float> PointCloud2Iter;
+#include <sensor_msgs/msg/point_field.hpp>
 #include <std_msgs/msg/float32.hpp>
 #include <visualization_msgs/msg/marker.hpp>
+typedef visualization_msgs::msg::Marker visMarker;
 #include <visualization_msgs/msg/marker_array.hpp>
 
 #define NOT_IMPLEMENTED                                                \
     std::cerr << "This function is not implemented yet!" << std::endl; \
     assert(false);
-#define PTS_PER_POLYGON 2048 // just cause that makes it more stable
+#define PTS_PER_POLYGON 2048  // just cause that makes it more stable
 typedef boost::lockfree::spsc_queue<Point,
                                     boost::lockfree::capacity<PTS_PER_POLYGON>>
     PointQueue;
@@ -36,17 +43,13 @@ class SupportPolygon : public Polygon {
         Polygon polygon;
         pts.consume_all(
             [&polygon](Point pt) { bg::append(polygon.outer(), pt); });
-        // if (comparePoints(pts[0], *(pts.end() - 1)))
-        //     bg::append(polygon.outer(), pts[0]);  // ring condition for
-        //     polygon
         bg::convex_hull(polygon, this->hullPolygon);
     }
 
-    void visualize(
-        std::shared_ptr<rclcpp::Publisher<visualization_msgs::msg::Marker>>
-            publisher) {
-        visualization_msgs::msg::Marker marker;
-        marker.header.frame_id = "odom";
+    void visualizePolygon(
+        std::shared_ptr<rclcpp::Publisher<visMarker>> pubPolygon) {
+        visMarker marker;
+        marker.header.frame_id = "pelvis";
         marker.type = 11;  // TRIANGLE_LIST
         marker.scale.x = 1;
         marker.scale.y = 1;
@@ -74,12 +77,69 @@ class SupportPolygon : public Polygon {
             }
             geoLastPt = geoPt;
         }
-        marker.color.r = 0;
-        marker.color.g = 0;
+        marker.color.r = 1;
+        marker.color.g = 1;
         marker.color.b = 1;
         marker.color.a = 0.7;
 
-        publisher->publish(marker);
+        pubPolygon->publish(marker);
+    }
+
+    void visualizeStability(
+        std::shared_ptr<rclcpp::Publisher<msgPointCloud2>> pub,
+        const uint gridLength = 50) {
+        float x0 = 0;
+        float x1 = 0;
+        float y0 = 0;
+        float y1 = 0;
+        for (const Point &p : this->hullPolygon.outer()) {
+            minmaxSetter(bg::get<0>(p), x0, x1);
+            minmaxSetter(bg::get<1>(p), y0, y1);
+        }
+        const float stepSizeX = (x1 - x0) / gridLength;
+        const float stepSizeY = (y1 - y0) / gridLength;
+
+        msgPointCloud2 msgPcl;
+        msgPcl.width = gridLength;
+        msgPcl.height = gridLength;
+        msgPcl.header.frame_id = "pelvis";
+        msgPcl.is_dense = false;
+
+        sensor_msgs::PointCloud2Modifier modifier(msgPcl);
+        modifier.setPointCloud2Fields(
+            4, "x", 1, sensor_msgs::msg::PointField::FLOAT32, "y", 1,
+            sensor_msgs::msg::PointField::FLOAT32, "z", 1,
+            sensor_msgs::msg::PointField::FLOAT32, "intensity", 1,
+            sensor_msgs::msg::PointField::FLOAT32);
+
+        PointCloud2Iter xIt(msgPcl, "x");
+        PointCloud2Iter yIt(msgPcl, "y");
+        PointCloud2Iter zIt(msgPcl, "z");
+        PointCloud2Iter vIt(msgPcl, "intensity");
+
+        std::srand(std::time({}));
+
+        for (float i = 0; i < gridLength; i++) {
+            for (float j = 0; j < gridLength; j++) {
+                const float x = x0 + i * stepSizeX;
+                const float y = y0 + j * stepSizeY;
+
+                Point simulatedCoM = Point(x, y);
+                float v = calculateStaticStability(simulatedCoM);
+                if (v < 0) continue;
+                *vIt = v;
+                *xIt = x;
+                *yIt = y;
+                *zIt = 0.0;
+
+                ++xIt;
+                ++yIt;
+                ++zIt;
+                ++vIt;
+            }
+        }
+
+        pub->publish(msgPcl);
     }
 
     bool centerOfMassInside(Point com) {
@@ -108,13 +168,20 @@ class SupportPolygon : public Polygon {
         }
         return dist;
     }
+
+    static void minmaxSetter(const float &v, float &out_min, float &out_max) {
+        out_min = std::min(v, out_min);
+        out_max = std::max(v, out_max);
+    }
 };
 
 void writeToBuffer(ros_gz_interfaces::msg::Contacts contacts, PointQueue &pts) {
     for (auto c : contacts.contacts) {
         for (auto pos : c.positions) {
             Point pt = Point(pos.x, pos.y);
-            while (!pts.push(pt)) { pts.reset(); }
+            while (!pts.push(pt)) {
+                pts.reset();
+            }
         }
     }
 }
@@ -147,8 +214,9 @@ int main(int argc, char **argv) {
                 com = Point(msgPt.point.x, msgPt.point.y);
             });
 
-    auto visPub =
-        node->create_publisher<visualization_msgs::msg::Marker>("vis_PoS", 10);
+    auto visPubPoly = node->create_publisher<visMarker>("vis_PoS", 10);
+    auto visPubStability =
+        node->create_publisher<msgPointCloud2>("vis_Stability", 10);
     auto stabilityPub =
         node->create_publisher<std_msgs::msg::Float32>("stability", 10);
     while (rclcpp::ok()) {
@@ -156,7 +224,8 @@ int main(int argc, char **argv) {
         if (pts.read_available() < PTS_PER_POLYGON) continue;
 
         SupportPolygon supPolygon = SupportPolygon(pts);
-        supPolygon.visualize(visPub);
+        supPolygon.visualizePolygon(visPubPoly);
+        supPolygon.visualizeStability(visPubStability);
 
         float stabilityMetric = supPolygon.calculateStaticStability(com);
         std_msgs::msg::Float32 stabilityMsg;
