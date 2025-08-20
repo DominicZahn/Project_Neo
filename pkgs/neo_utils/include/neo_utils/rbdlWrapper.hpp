6693804a9727eb37d899500564ce0f7b030e145a
Dominic Zahn <dominic.zahn@iosb.fraunhofer.de>
2025-08-20 17:24:02 +0200

    Implemtend passiv part of rbdl wrapper

---
+++ pkgs/neo_utils/include/neo_utils/rbdlWrapper.hpp
@@ -0,0 +1,59 @@
+#pragma once
+
+// ------------------- rbdl ------------------
+#include <rbdl/Body.h>
+#include <rbdl/Joint.h>
+#include <rbdl/Kinematics.h>
+#include <rbdl/Model.h>
+#ifndef RBDL_BUILD_ADDON_URDFREADER
+#error "Error: RBDL Addon URDFReader not enabled."
+#endif
+#include <rbdl/addons/urdfreader/urdfreader.h>
+
+using namespace RigidBodyDynamics;
+using namespace RigidBodyDynamics::Math;
+
+// ------------------- ros2 -------------------
+#include <rclcpp/parameter_client.hpp>
+#include <rclcpp/rclcpp.hpp>
+#include <sensor_msgs/msg/joint_state.hpp>
+typedef sensor_msgs::msg::JointState MsgJointState;
+
+// ----------------- general C++ --------------
+#include <cmath>
+#include <memory>
+#include <unordered_map>
+
+using namespace std::chrono_literals;
+
+namespace neo_utils {
+/**
+ * @class RBDLWrapper
+ * A wrapper for rbdl that also stays consistent with the current ros2 state.
+ * It therefore depends on the following processes already running:
+ * - /joint_states (topic)
+ * - /robot_state_publisher/robot_description (parameter)
+ */
+class RBDLWrapper {
+   public:
+    RBDLWrapper(bool floatingBase = false);
+    ~RBDLWrapper();
+    // ------ getter for exposed members ------
+    std::shared_ptr<const Model> get_model();
+    const VectorNd &get_q() const;
+    const VectorNd &get_qdot() const;
+
+   private:
+    // ---------- internal members -------------
+    std::shared_ptr<rclcpp::Node> node;
+    std::shared_ptr<rclcpp::Subscription<MsgJointState>> jointStateSub;
+    std::unordered_map<std::string, uint> jointState2JointIdx;
+    // ---------- exposed members --------------
+    std::shared_ptr<Model> model;
+    VectorNd q;
+    VectorNd qdot;
+    // ---------- private functions ------------
+    const std::string retrieveUrdf();
+    void updateJointStates(MsgJointState msg);
+};
+}  // namespace neo_utils
---
+++ pkgs/neo_utils/src/rbdlWrapper.cpp
@@ -0,0 +1,63 @@
+#include <neo_utils/rbdlWrapper.hpp>
+
+neo_utils::RBDLWrapper::RBDLWrapper(bool floatingBase) {
+    node = std::make_shared<rclcpp::Node>("rbdl_wrapper");
+    const std::string &urdfStr = retrieveUrdf();
+    model = std::make_shared<Model>();
+    Addons::URDFReadFromString(urdfStr.c_str(), model.get(), floatingBase);
+
+    auto jointStateUpdate = [this](MsgJointState jointState) {
+        this->updateJointStates(jointState);
+    };
+    jointStateSub = node->create_subscription<MsgJointState>(
+        "/joint_states", 10, jointStateUpdate);
+}
+
+neo_utils::RBDLWrapper::~RBDLWrapper() {}
+
+std::shared_ptr<const Model> neo_utils::RBDLWrapper::get_model() {
+    return model;
+}
+
+const VectorNd &neo_utils::RBDLWrapper::get_q() const { return q; }
+
+const VectorNd &neo_utils::RBDLWrapper::get_qdot() const { return qdot; }
+
+const std::string neo_utils::RBDLWrapper::retrieveUrdf() {
+    auto paramClientNode = std::make_shared<rclcpp::SyncParametersClient>(
+        node, "robot_state_publisher");
+    while (!paramClientNode->wait_for_service(std::chrono::seconds(1))) {
+    }
+
+    return paramClientNode->get_parameter<std::string>("robot_description");
+}
+
+void neo_utils::RBDLWrapper::updateJointStates(MsgJointState msg) {
+    for (uint jointStateIdx = 0; jointStateIdx < msg.name.size();
+         jointStateIdx++) {
+        std::string name = msg.name[jointStateIdx];
+        auto mapIter = jointState2JointIdx.find(name);
+        uint q_idx = -1;
+        if (mapIter != jointState2JointIdx.end()) {
+            // retrive from map
+            q_idx = mapIter->second;
+        } else {
+            // retrive from mBodies and save to map
+            int wordPos = name.find("joint");
+            if (wordPos >= 0) {
+                name.replace(wordPos, std::string("joint").length(), "link");
+            }
+            // find body with same name, without "joint", to select correct
+            // link link and joint have the same id in rbdl => bodyId =
+            // jointId
+            uint bodyId = model->GetBodyId(name.c_str());
+            if (bodyId > model->mBodies.size()) continue;
+            q_idx = model->mJoints[bodyId].q_index;
+
+            jointState2JointIdx.insert({msg.name[jointStateIdx], q_idx});
+        }
+        q[q_idx] = msg.position[jointStateIdx];
+        qdot[q_idx] =
+            msg.velocity.size() > 0 ? msg.velocity[jointStateIdx] : 0.0;
+    }
+}
