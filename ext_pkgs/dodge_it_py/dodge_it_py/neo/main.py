5ae2b37ba2fbaffee4a6a0c8a66499d407132484
DominicZahn <zahndominic@web.de>
2026-09-02 15:44:00 +0200

    [STABLE] benchmark run with fibonacciThomson sampling

--- ext_pkgs/dodge_it_py/dodge_it_py/neo/main.py
+++ ext_pkgs/dodge_it_py/dodge_it_py/neo/main.py
@@ -154,7 +154,7 @@ def mainBenchmark(sampleCount : int):
         radius,
         -0.1, 0.4 
     )
-    projectilePosArr, normalArr = hule.sample(sampleCount)
+    projectilePosArr, normalArr = hule.sampleFibonacciThomson(sampleCount, 0)
     ptsCentered = projectilePosArr - np.array(center)
     v = 2*(np.sum(ptsCentered**2/np.array(radius)**4, axis=1))**(3/2) / (np.sum(ptsCentered**2/np.array(radius)**6, axis=1)) / Tf
     projectileVelArr = -normalArr * v[:,None]
